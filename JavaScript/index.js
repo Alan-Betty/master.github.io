53b8f1f8c0e1b724bdd574bc231382ef2d81b210
Alan Betty <137591870+Alan-Betty@users.noreply.github.com>
2024-05-06 12:42:27 +0530

    Delete JavaScript directory

--- JavaScript/index.js
+++
@@ -1,7 +0,0 @@
-// typing Animation
-var typed = new Typed(".typing",{
-    strings:[" ","Web Designer","Web Developer","Video Editor","YouTuber"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
